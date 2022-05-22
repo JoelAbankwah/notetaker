9cd0403325fe7d47c6de50ed1528372ee740102f
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-05-21 21:49:40 -0700

    add modularized route files

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = 3001;
+const app = express();
+const apiRoutes = require('./Develop/routes/apiRoutes/noteRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes/index');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/Develop/public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
