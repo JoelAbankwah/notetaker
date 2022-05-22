e18521e99577e621fd454377bdb3150ecad9b1a1
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-05-21 21:59:50 -0700

    updated port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./Develop/routes/apiRoutes/noteRoutes');
 const htmlRoutes = require('./Develop/routes/htmlRoutes/index');
